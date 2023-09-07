274d7f3db731e4be0ed3630b7577f11409b3bec6
notunderctrl <avraj@underctrl.io>
2023-09-07 15:47:39 +0300

    update permissions check to show missing permissions

--- packages/commandkit/src/handlers/command-handler/validations/botPermissions.ts
+++ packages/commandkit/src/handlers/command-handler/validations/botPermissions.ts
@@ -4,17 +4,25 @@ export default function ({ interaction, targetCommand }: BuiltInValidationParams
     const botMember = interaction.guild?.members.me;
 
     if (targetCommand.options?.botPermissions && botMember) {
+        const missingPermissions: string[] = [];
+
         for (const permission of targetCommand.options.botPermissions) {
             const hasPermission = botMember.permissions.has(permission);
 
             if (!hasPermission) {
-                interaction.reply({
-                    content: `❌ I do not have enough permission to execute this command. Required permission: \`${permission}\``,
-                    ephemeral: true,
-                });
-
-                return true;
+                missingPermissions.push(`\`${permission.toString()}\``);
             }
+        }
+
+        if (missingPermissions.length) {
+            interaction.reply({
+                content: `❌ I do not have enough permission to execute this command. Missing: ${missingPermissions.join(
+                    ', ',
+                )}`,
+                ephemeral: true,
+            });
+
+            return true;
         }
     }
 }
--- packages/commandkit/src/handlers/command-handler/validations/userPermissions.ts
+++ packages/commandkit/src/handlers/command-handler/validations/userPermissions.ts
@@ -4,17 +4,25 @@ export default function ({ interaction, targetCommand }: BuiltInValidationParams
     const memberPermissions = interaction.memberPermissions;
 
     if (targetCommand.options?.userPermissions && memberPermissions) {
+        const missingPermissions: string[] = [];
+
         for (const permission of targetCommand.options.userPermissions) {
             const hasPermission = memberPermissions.has(permission);
 
             if (!hasPermission) {
-                interaction.reply({
-                    content: `❌ You do not have enough permission to run this command. Required permission: \`${permission}\``,
-                    ephemeral: true,
-                });
-
-                return true;
+                missingPermissions.push(`\`${permission.toString()}\``);
             }
+        }
+
+        if (missingPermissions.length) {
+            interaction.reply({
+                content: `❌ You do not have enough permission to run this command. Missing: ${missingPermissions.join(
+                    ', ',
+                )}`,
+                ephemeral: true,
+            });
+
+            return true;
         }
     }
 }
