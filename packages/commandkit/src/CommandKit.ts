932ea65cdc9a62181e0234183534560a8f398c79
notunderctrl <avraj@underctrl.io>
2023-09-20 20:12:14 +0300

    add getters for commandkit's initial props

--- packages/commandkit/src/CommandKit.ts
+++ packages/commandkit/src/CommandKit.ts
@@ -63,11 +63,7 @@ export class CommandKit {
         }
     }
 
-    /**
-     * Returns all the commands that CommandKit is handling.
-     *
-     * @returns An array of command objects
-     */
+    /** @returns An array of objects of all the commands that CommandKit is handling. */
     get commands(): CommandObject[] {
         const commands = this.#data.commands.map((cmd) => {
             const { run, ...command } = cmd;
@@ -75,5 +71,35 @@ export class CommandKit {
         });
 
         return commands;
+    }
+
+    /** @returns The path to the commands folder which was set when instantiating CommandKit. */
+    get commandsPath(): string | undefined {
+        return this.#data.commandsPath;
+    }
+
+    /** @returns The path to the events folder which was set when instantiating CommandKit. */
+    get eventsPath(): string | undefined {
+        return this.#data.eventsPath;
+    }
+
+    /** @returns The path to the validations folder which was set when instantiating CommandKit. */
+    get validationsPath(): string | undefined {
+        return this.#data.validationsPath;
+    }
+
+    /** @returns An array of all the developer user IDs which was set when instantiating CommandKit. */
+    get devUserIds(): string[] {
+        return this.#data.devUserIds || [];
+    }
+
+    /** @returns An array of all the developer guild IDs which was set when instantiating CommandKit. */
+    get devGuildIds(): string[] {
+        return this.#data.devGuildIds || [];
+    }
+
+    /** @returns An array of all the developer role IDs which was set when instantiating CommandKit. */
+    get devRoleIds(): string[] {
+        return this.#data.devRoleIds || [];
     }
 }
