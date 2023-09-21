438737aa4082cfe10a38d253bd707ec3aa816dcd
notunderctrl <avraj@underctrl.io>
2023-09-21 11:33:45 +0300

    docs: update config + structure

--- apps/nextra-docs/theme.config.tsx
+++ apps/nextra-docs/theme.config.tsx
@@ -1,6 +1,20 @@
-export default {
+import type { DocsThemeConfig } from 'nextra-theme-docs';
+import { useRouter } from 'next/router';
+
+const config: DocsThemeConfig = {
     logo: <span>CommandKit</span>,
     project: {
         link: 'https://github.com/underctrl-io/commandkit',
     },
+    docsRepositoryBase: 'https://github.com/underctrl-io/commandkit/blob/apps/docs',
+    useNextSeoProps() {
+        const { asPath } = useRouter();
+        if (asPath !== '/') {
+            return {
+                titleTemplate: '%s – CommandKit',
+            };
+        }
+    },
 };
+
+export default config;
