1ab8ec77d09c0d94d73a679c370c82c6e9b9fc69
FearlessNite345 <110247392+FearlessNite345@users.noreply.github.com>
2023-10-09 10:28:00 -0400

    command testing complete and working

--- packages/commandkit/tests/commands/misc/ping.ts
+++ packages/commandkit/tests/commands/misc/ping.ts
@@ -1,6 +1,7 @@
+import { SlashCommandBuilder } from 'discord.js';
 import { type SlashCommandProps, type CommandOptions, type CommandData, BasicSlashCommand, UserContextCommand, CommandType } from '../../../src/index';
 
-export const data: CommandData = {
+/*export const data: CommandData = {
     name: 'ping',
     description: 'Pong!',
 };
@@ -11,4 +12,18 @@ export async function run({ interaction, client, handler }: SlashCommandProps) {
 
 export const options: CommandOptions = {
     devOnly: true,
-};
+};*/
+
+export default new BasicSlashCommand({
+    run: ({ interaction, client }) => {
+        interaction.reply(`:ping_pong: Pong! \`${client.ws.ping}ms\``);
+    },
+
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Pong!'),
+
+    options: {
+        devOnly: true
+    }
+})
--- packages/commandkit/tests/index.ts
+++ packages/commandkit/tests/index.ts
@@ -13,8 +13,8 @@ new CommandKit({
     commandsPath: `${__dirname}/commands`,
     eventsPath: `${__dirname}/events`,
     validationsPath: `${__dirname}/validations`,
-    devGuildIds: ['1049345075366334617'],
-    devUserIds: ['1049343381903515778'],
+    devGuildIds: ['1004792054783688775'], //'1049345075366334617'
+    devUserIds: ['1002401206750150836'], //'1049343381903515778'
     bulkRegister: true,
 });
 
