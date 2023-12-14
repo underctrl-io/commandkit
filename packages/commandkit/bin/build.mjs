6d50519f70f5a249077f5eb838c082051bc7e7d2
skdhg <46562212+skdhg@users.noreply.github.com>
2023-12-14 23:31:42 +0545

    chore: prettier

--- packages/commandkit/bin/build.mjs
+++ packages/commandkit/bin/build.mjs
@@ -64,40 +64,40 @@ export async function injectShims(outDir, main, antiCrash, polyfillRequire) {
     const tail = '\n})();';
     const requireScript = polyfillRequire
         ? [
-            '// --- CommandKit require() polyfill ---',
-            '  if (typeof require === "undefined") {',
-            '    const { createRequire } = await import("node:module");',
-            '    const __require = createRequire(import.meta.url);',
-            '    Object.defineProperty(globalThis, "require", {',
-            '      value: (id) => {',
-            '        return __require(id);',
-            '      },',
-            '      configurable: true,',
-            '      enumerable: false,',
-            '      writable: true,',
-            '    });',
-            '  }',
-            '// --- CommandKit require() polyfill ---',
-        ].join('\n')
+              '// --- CommandKit require() polyfill ---',
+              '  if (typeof require === "undefined") {',
+              '    const { createRequire } = await import("node:module");',
+              '    const __require = createRequire(import.meta.url);',
+              '    Object.defineProperty(globalThis, "require", {',
+              '      value: (id) => {',
+              '        return __require(id);',
+              '      },',
+              '      configurable: true,',
+              '      enumerable: false,',
+              '      writable: true,',
+              '    });',
+              '  }',
+              '// --- CommandKit require() polyfill ---',
+          ].join('\n')
         : '';
 
     const antiCrashScript = antiCrash
         ? [
-            '// --- CommandKit Anti-Crash Monitor ---',
-            "  // 'uncaughtException' event is supposed to be used to perform synchronous cleanup before shutting down the process",
-            '  // instead of using it as a means to resume operation.',
-            '  // But it exists here due to compatibility reasons with discord bot ecosystem.',
-            "  const p = (t) => `\\x1b[33m${t}\\x1b[0m`, b = '[CommandKit Anti-Crash Monitor]', l = console.log, e1 = 'uncaughtException', e2 = 'unhandledRejection';",
-            '  if (!process.eventNames().includes(e1)) // skip if it is already handled',
-            '    process.on(e1, (e) => {',
-            '      l(p(`${b} Uncaught Exception`)); l(p(b), p(e.stack || e));',
-            '    })',
-            '  if (!process.eventNames().includes(e2)) // skip if it is already handled',
-            '    process.on(e2, (r) => {',
-            '      l(p(`${b} Unhandled promise rejection`)); l(p(`${b} ${r.stack || r}`));',
-            '    });',
-            '// --- CommandKit Anti-Crash Monitor ---',
-        ].join('\n')
+              '// --- CommandKit Anti-Crash Monitor ---',
+              "  // 'uncaughtException' event is supposed to be used to perform synchronous cleanup before shutting down the process",
+              '  // instead of using it as a means to resume operation.',
+              '  // But it exists here due to compatibility reasons with discord bot ecosystem.',
+              "  const p = (t) => `\\x1b[33m${t}\\x1b[0m`, b = '[CommandKit Anti-Crash Monitor]', l = console.log, e1 = 'uncaughtException', e2 = 'unhandledRejection';",
+              '  if (!process.eventNames().includes(e1)) // skip if it is already handled',
+              '    process.on(e1, (e) => {',
+              '      l(p(`${b} Uncaught Exception`)); l(p(b), p(e.stack || e));',
+              '    })',
+              '  if (!process.eventNames().includes(e2)) // skip if it is already handled',
+              '    process.on(e2, (r) => {',
+              '      l(p(`${b} Unhandled promise rejection`)); l(p(`${b} ${r.stack || r}`));',
+              '    });',
+              '// --- CommandKit Anti-Crash Monitor ---',
+          ].join('\n')
         : '';
 
     const contents = await readFile(path, 'utf-8');
