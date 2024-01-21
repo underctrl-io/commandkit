1d372dbfde531995085aee36956d5887ab4c60a8
M1 <140819896+m1-dev@users.noreply.github.com>
2024-01-21 17:09:43 +0200

    build: minify for optimization (#52)
    * build: minify for optimization
    
    * build: disable minification for CLI command
    
    * build: disable minifying except for identifiers
    
    * chore(build): enable keepNames

--- packages/commandkit/tsup.config.ts
+++ packages/commandkit/tsup.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
     format: ['cjs', 'esm'],
     entry: ['./src/index.ts'],
+    minifyIdentifiers: false,
+    minifySyntax: true,
+    minifyWhitespace: true,
+    keepNames: true,
     dts: true,
     shims: true,
     skipNodeModulesBundle: true,
