5f902fc9928abb2a1fcf18bceacb1326d4cdd70a
EndlessM0DZ <tyytofficial@gmail.com>
2024-05-17 18:49:42 -0400

    Update checkDuplicateCommands.ts
    Array.prototype.join is now being used in the "warnDuplicates" function to avoid nested loops. There is a new function "commandType" to help identify the commands. "checkDuplicateCommands" is now more simplified by creating an obj and adding the CommandFileObjects to an array based on a common command name. Then a loop to push any keys with more than one CommandFileObject. Also if there is only one CommandFileObject in a key it will push that CommandFileObject and not an array of just that one to avoid unnecessary arrays nesting. Because of this change duplicates have been renamed to duplicatePairsArray and nonduplicates have been renamed to nonduplicateArray.

--- packages/commandkit/src/handlers/command-handler/functions/checkDuplicateCommands.ts
+++ packages/commandkit/src/handlers/command-handler/functions/checkDuplicateCommands.ts
@@ -1,34 +1,30 @@
 import { CommandFileObject } from '../../../typings';
 import colors  from '../../../utils/colors'
-export function checkDuplicateCommands(commands: CommandFileObject[]){
-    let tempCommands = [...commands], duplicates = [], nonduplicates = [];
-    for(let i = 0; tempCommands.length != 0;){
-      let indexes = []
-       let duplicatePairs = tempCommands.filter((obj, index) => {
-        if(obj.data.name === tempCommands[i].data.name){
-          indexes.push(index)
-          return true
-        }
-        else return false
-       })
-       if(duplicatePairs.length > 1) duplicates.push(duplicatePairs)
-       else nonduplicates.push(duplicatePairs)
-       for(let j = indexes.length - 1; j > -1; j--){
-         tempCommands.splice(indexes[j], 1)
-       }
+export function checkDuplicateCommands(commands: CommandFileObject[]) {
+    const commandObj: { [key: string]: CommandFileObject[] } = {}, duplicatePairsArray: CommandFileObject[][] = [], nonduplicateArray: CommandFileObject[] = [];
+    commands.forEach(command => {
+        const name = command.data.name;
+        if (!commandObj[name]) commandObj[name] = []
+        commandObj[name].push(command);
+    });
+    for (const key in commandObj) {
+        if (commandObj[key].length > 1) duplicatePairsArray.push(commandObj[key]) 
+        else nonduplicateArray.push(commandObj[key][0])
     }
     return {
-        duplicates,
-        nonduplicates
-    }
-  }
+        duplicatePairsArray,
+        nonduplicateArray
+    };
+}
+function commandType(command: CommandFileObject) {
+    return command.options.devOnly ? "(devOnly) dm_permission: false" : `(global) dm_permission: ${(command.data.dm_permission !== false)}`;
+}
 export function warnDuplicatesIfAny(commands: CommandFileObject[]){
-  let { duplicates, nonduplicates } = checkDuplicateCommands(commands)
-  for(let i = 0; i < duplicates.length; i++){
-    let myString = `You have duplicate command names with command: ${colors.red(duplicates[i][0].data.name)} in the following files:`
-    for(let j = 0; j < duplicates[i].length; j++){
-        myString += `\n${duplicates[i][j].filePath}`
-    }
-    console.log(colors.yellow('[WARNING]'), myString)
-  }
+    let { duplicatePairsArray, nonduplicateArray } = checkDuplicateCommands(commands);
+    duplicatePairsArray.forEach(duplicatePair => {
+      const commandName = colors.red(duplicatePair[0].data.name);
+      const fileDetails = duplicatePair.map((dup: CommandFileObject) => `${dup.filePath} [${commandType(dup)}]`).join('\n');
+      const warningMessage = `You have duplicate command names with command: ${commandName} in the following files:\n${fileDetails}`;
+      console.log(colors.yellow('[WARNING]'), warningMessage);
+    });
 }
