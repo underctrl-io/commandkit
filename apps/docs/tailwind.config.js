7852488a72b9f4896b3f3183251db1ddf157117b
notunderctrl <avraj@underctrl.io>
2024-12-15 13:18:01 +0300

    update docs theme + fix guide sorting

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -10,5 +10,9 @@ export default {
     './node_modules/fumadocs-ui/dist/**/*.js',
     '../../node_modules/fumadocs-ui/dist/**/*.js',
   ],
-  presets: [createPreset()],
+  presets: [
+    createPreset({
+      preset: 'vitepress',
+    }),
+  ],
 };
