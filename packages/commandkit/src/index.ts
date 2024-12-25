157078ee7b1e4281311ecd42f7ef276dfd6d9e66
twlite <46562212+twlite@users.noreply.github.com>
2024-12-25 12:25:53 +0545

    chore: replace version inject plugin with macro plugin

--- packages/commandkit/src/index.ts
+++ packages/commandkit/src/index.ts
@@ -3,9 +3,12 @@ export * from './components';
 export * from './config';
 export type * from './types';
 
+function getCommandKitVersion(): string {
+  'use macro';
+  return require('../package.json').version;
+}
+
 /**
  * The current version of CommandKit.
  */
-// Note to developers: This needs to explicitly be `string` so it is not typed as a "const string" that gets injected by esbuild
-// eslint-disable-next-line @typescript-eslint/no-inferrable-types
-export const version: string = '[VI]{{inject}}[/VI]';
+export const version = getCommandKitVersion();
