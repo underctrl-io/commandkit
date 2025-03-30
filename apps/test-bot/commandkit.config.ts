204f46a88428de32d1ec3f705c6fe4b828e9c7fb
Twilight <46562212+twlite@users.noreply.github.com>
2025-03-30 19:14:51 +0545

    chore: update commandkit.config.ts

--- apps/test-bot/commandkit.config.ts
+++ apps/test-bot/commandkit.config.ts
@@ -1,27 +1,5 @@
-import {
-  CommandKitPluginRuntime,
-  defineConfig,
-  Logger,
-  RuntimePlugin,
-} from 'commandkit';
-
-class ExtendedLogsPlugin extends RuntimePlugin {
-  name = 'ExtendedLogsPlugin';
-
-  async activate(ctx: CommandKitPluginRuntime): Promise<void> {
-    ctx.commandkit.client.on('typingStart', (typing) => {
-      ctx.commandkit.events.to('extended').emit('messageTyping', typing);
-    });
-
-    Logger.info('ExtendedLogsPlugin activated');
-  }
-
-  async deactivate(ctx: CommandKitPluginRuntime): Promise<void> {
-    ctx.commandkit.client.removeAllListeners('typingStart');
-  }
-}
+import { defineConfig } from 'commandkit';
 
 export default defineConfig({
-  plugins: [new ExtendedLogsPlugin({})],
   typedLocales: true,
 });
