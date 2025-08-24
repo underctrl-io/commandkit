a25d29f94ac39c3bbf3690d6c0542b6a1ad0f2db
twlite <46562212+twlite@users.noreply.github.com>
2025-08-24 10:56:02 +0545

    feat: force run env finalizer during runCommand phase

--- packages/commandkit/src/app/commands/AppCommandRunner.ts
+++ packages/commandkit/src/app/commands/AppCommandRunner.ts
@@ -75,90 +75,113 @@ export class AppCommandRunner {
     env.variables.set('execHandlerKind', executionMode);
     env.variables.set('customHandler', options?.handler ?? null);
 
-    const middlewareCtx = new MiddlewareContext(commandkit, {
-      command: prepared.command,
-      environment: env,
-      executionMode,
-      interaction: !(source instanceof Message)
-        ? (source as ChatInputCommandInteraction)
-        : (null as never),
-      message: source instanceof Message ? source : (null as never),
-      forwarded: false,
-      customArgs: {
-        setCommandRunner: (fn: RunCommand) => {
-          runCommand = fn;
+    try {
+      const middlewareCtx = new MiddlewareContext(commandkit, {
+        command: prepared.command,
+        environment: env,
+        executionMode,
+        interaction: !(source instanceof Message)
+          ? (source as ChatInputCommandInteraction)
+          : (null as never),
+        message: source instanceof Message ? source : (null as never),
+        forwarded: false,
+        customArgs: {
+          setCommandRunner: (fn: RunCommand) => {
+            runCommand = fn;
+          },
         },
-      },
-      messageCommandParser: prepared.messageCommandParser,
-    });
-
-    const beforeMiddlewares = prepared.middlewares.filter(
-      (m) => m.data.beforeExecute,
-    );
-
-    let beforeMiddlewaresStopped = false;
-
-    // Run middleware before command execution
-    if (beforeMiddlewares.length) {
-      await provideContext(env, async () => {
-        for (const middleware of beforeMiddlewares) {
-          try {
-            await middleware.data.beforeExecute(middlewareCtx);
-          } catch (e) {
-            if (isErrorType(e, CommandKitErrorCodes.StopMiddlewares)) {
-              beforeMiddlewaresStopped = true;
-              Logger.debug(
-                `Middleware propagation stopped for command "${middlewareCtx.commandName}". stopMiddlewares() was called inside a beforeExecute function at "${middleware.middleware.relativePath}"`,
-              );
-              break; // Stop the middleware loop if `stopMiddlewares()` is called.
-            }
-
-            if (
-              isErrorType(e, [
-                CommandKitErrorCodes.ForwardedCommand,
-                CommandKitErrorCodes.InvalidCommandPrefix,
-              ])
-            ) {
-              continue;
-            }
-
-            throw e;
-          }
-        }
+        messageCommandParser: prepared.messageCommandParser,
       });
-    }
 
-    let result: any;
+      const beforeMiddlewares = prepared.middlewares.filter(
+        (m) => m.data.beforeExecute,
+      );
 
-    let stopMiddlewaresCalledInCmd = false;
+      let beforeMiddlewaresStopped = false;
 
-    // If no `stopMiddlewares()` was called in a `beforeExecute` middleware, try to run the command
-    if (!beforeMiddlewaresStopped) {
-      const targetData = prepared.command.data;
-      const fn = targetData[options?.handler || executionMode];
+      // Run middleware before command execution
+      if (beforeMiddlewares.length) {
+        await provideContext(env, async () => {
+          for (const middleware of beforeMiddlewares) {
+            try {
+              await middleware.data.beforeExecute(middlewareCtx);
+            } catch (e) {
+              if (isErrorType(e, CommandKitErrorCodes.StopMiddlewares)) {
+                beforeMiddlewaresStopped = true;
+                Logger.debug(
+                  `Middleware propagation stopped for command "${middlewareCtx.commandName}". stopMiddlewares() was called inside a beforeExecute function at "${middleware.middleware.relativePath}"`,
+                );
+                break; // Stop the middleware loop if `stopMiddlewares()` is called.
+              }
 
-      if (!fn) {
-        Logger.warn(
-          `Command ${prepared.command.command.name} has no handler for ${executionMode}`,
-        );
+              if (
+                isErrorType(e, [
+                  CommandKitErrorCodes.ForwardedCommand,
+                  CommandKitErrorCodes.InvalidCommandPrefix,
+                ])
+              ) {
+                continue;
+              }
+
+              throw e;
+            }
+          }
+        });
       }
 
-      const analytics = commandkit.analytics;
+      let result: any;
 
-      if (fn) {
-        try {
-          const _executeCommand = makeContextAwareFunction(
-            env,
-            async () => {
-              env.registerDeferredFunction(async (env) => {
-                env.markEnd();
-                const error = env.getExecutionError();
-                const marker = env.getMarker();
-                const time = `${env.getExecutionTime().toFixed(2)}ms`;
+      let stopMiddlewaresCalledInCmd = false;
 
-                if (error) {
-                  Logger.error(
-                    `[${marker} - ${time}] Error executing command: ${error.stack || error}`,
+      // If no `stopMiddlewares()` was called in a `beforeExecute` middleware, try to run the command
+      if (!beforeMiddlewaresStopped) {
+        const targetData = prepared.command.data;
+        const fn = targetData[options?.handler || executionMode];
+
+        if (!fn) {
+          Logger.warn(
+            `Command ${prepared.command.command.name} has no handler for ${executionMode}`,
+          );
+        }
+
+        const analytics = commandkit.analytics;
+
+        if (fn) {
+          try {
+            const _executeCommand = makeContextAwareFunction(
+              env,
+              async () => {
+                env.registerDeferredFunction(async (env) => {
+                  env.markEnd();
+                  const error = env.getExecutionError();
+                  const marker = env.getMarker();
+                  const time = `${env.getExecutionTime().toFixed(2)}ms`;
+
+                  if (error) {
+                    Logger.error(
+                      `[${marker} - ${time}] Error executing command: ${error.stack || error}`,
+                    );
+
+                    const commandName =
+                      prepared.command?.data?.command?.name ??
+                      prepared.command.command.name;
+
+                    await analytics.track({
+                      name: AnalyticsEvents.COMMAND_EXECUTION,
+                      id: commandName,
+                      data: {
+                        error: true,
+                        executionTime: env.getExecutionTime().toFixed(2),
+                        type: executionMode,
+                        command: commandName,
+                      },
+                    });
+
+                    return;
+                  }
+
+                  Logger.info(
+                    `[${marker} - ${time}] Command executed successfully`,
                   );
 
                   const commandName =
@@ -169,112 +192,95 @@ export class AppCommandRunner {
                     name: AnalyticsEvents.COMMAND_EXECUTION,
                     id: commandName,
                     data: {
-                      error: true,
+                      error: false,
                       executionTime: env.getExecutionTime().toFixed(2),
                       type: executionMode,
                       command: commandName,
                     },
                   });
-
-                  return;
-                }
-
-                Logger.info(
-                  `[${marker} - ${time}] Command executed successfully`,
-                );
-
-                const commandName =
-                  prepared.command?.data?.command?.name ??
-                  prepared.command.command.name;
-
-                await analytics.track({
-                  name: AnalyticsEvents.COMMAND_EXECUTION,
-                  id: commandName,
-                  data: {
-                    error: false,
-                    executionTime: env.getExecutionTime().toFixed(2),
-                    type: executionMode,
-                    command: commandName,
-                  },
                 });
-              });
 
-              return fn(middlewareCtx.clone());
-            },
-            this.#finalizer.bind(this),
-          );
-
-          const executeCommand =
-            runCommand != null
-              ? (runCommand as RunCommand)(_executeCommand)
-              : _executeCommand;
-
-          env.markStart(prepared.command.data.command.name);
-
-          const res = await commandkit.plugins.execute(async (ctx, plugin) => {
-            return plugin.executeCommand(
-              ctx,
-              env,
-              source,
-              prepared,
-              executeCommand,
+                return fn(middlewareCtx.clone());
+              },
+              this.#finalizer.bind(this),
             );
-          });
 
-          if (!res) {
-            result = await executeCommand();
-          }
-        } catch (e) {
-          if (isErrorType(e, CommandKitErrorCodes.StopMiddlewares)) {
-            stopMiddlewaresCalledInCmd = true;
-            Logger.debug(
-              `Middleware propagation stopped for command "${middlewareCtx.commandName}". stopMiddlewares() was called by the command itself`,
+            const executeCommand =
+              runCommand != null
+                ? (runCommand as RunCommand)(_executeCommand)
+                : _executeCommand;
+
+            env.markStart(prepared.command.data.command.name);
+
+            const res = await commandkit.plugins.execute(
+              async (ctx, plugin) => {
+                return plugin.executeCommand(
+                  ctx,
+                  env,
+                  source,
+                  prepared,
+                  executeCommand,
+                );
+              },
             );
-          } else if (!isErrorType(e, CommandKitErrorCodes.ForwardedCommand)) {
-            if (shouldThrowOnError) {
-              throw e;
+
+            if (!res) {
+              result = await executeCommand();
             }
-            Logger.error(e);
-          }
-        }
-      }
-    } else {
-      result = {
-        error: true,
-        message:
-          'Command execution was cancelled by a beforeExecute middleware.',
-      };
-    }
-
-    const afterMiddlewares = prepared.middlewares.filter(
-      (m) => m.data.afterExecute,
-    );
-
-    // Run middleware after command execution only if `stopMiddlewares()` wasn't
-    // called in either `beforeExecute` middleware or in the command itself.
-    if (
-      !beforeMiddlewaresStopped &&
-      !stopMiddlewaresCalledInCmd &&
-      afterMiddlewares.length
-    ) {
-      await provideContext(env, async () => {
-        for (const middleware of afterMiddlewares) {
-          try {
-            await middleware.data.afterExecute(middlewareCtx);
           } catch (e) {
             if (isErrorType(e, CommandKitErrorCodes.StopMiddlewares)) {
+              stopMiddlewaresCalledInCmd = true;
               Logger.debug(
-                `Middleware propagation stopped for command "${middlewareCtx.commandName}". stopMiddlewares() was called inside an afterExecute function at "${middleware.middleware.relativePath}"`,
+                `Middleware propagation stopped for command "${middlewareCtx.commandName}". stopMiddlewares() was called by the command itself`,
               );
-              break; // Stop the afterExecute middleware loop if `stopMiddlewares()` is called.
+            } else if (!isErrorType(e, CommandKitErrorCodes.ForwardedCommand)) {
+              if (shouldThrowOnError) {
+                throw e;
+              }
+              Logger.error(e);
             }
-            throw e;
           }
         }
-      });
-    }
+      } else {
+        result = {
+          error: true,
+          message:
+            'Command execution was cancelled by a beforeExecute middleware.',
+        };
+      }
 
-    return result;
+      const afterMiddlewares = prepared.middlewares.filter(
+        (m) => m.data.afterExecute,
+      );
+
+      // Run middleware after command execution only if `stopMiddlewares()` wasn't
+      // called in either `beforeExecute` middleware or in the command itself.
+      if (
+        !beforeMiddlewaresStopped &&
+        !stopMiddlewaresCalledInCmd &&
+        afterMiddlewares.length
+      ) {
+        await provideContext(env, async () => {
+          for (const middleware of afterMiddlewares) {
+            try {
+              await middleware.data.afterExecute(middlewareCtx);
+            } catch (e) {
+              if (isErrorType(e, CommandKitErrorCodes.StopMiddlewares)) {
+                Logger.debug(
+                  `Middleware propagation stopped for command "${middlewareCtx.commandName}". stopMiddlewares() was called inside an afterExecute function at "${middleware.middleware.relativePath}"`,
+                );
+                break; // Stop the afterExecute middleware loop if `stopMiddlewares()` is called.
+              }
+              throw e;
+            }
+          }
+        });
+      }
+
+      return result;
+    } finally {
+      await this.#finalizer(env, false);
+    }
   }
 
   /**
@@ -282,17 +288,19 @@ export class AppCommandRunner {
    * @internal
    * Finalizes command execution by running deferred functions and plugin cleanup.
    */
-  async #finalizer() {
-    const env = useEnvironment();
+  async #finalizer(env?: CommandKitEnvironment, runPlugins = true) {
+    env ??= useEnvironment();
 
     await env.runDeferredFunctions();
 
     env.clearAllDeferredFunctions();
 
     // plugins may have their own deferred function, useful for cleanup or post-command analytics
-    await this.handler.commandkit.plugins.execute(async (ctx, plugin) => {
-      await plugin.onAfterCommand(ctx, env);
-    });
+    if (runPlugins) {
+      await this.handler.commandkit.plugins.execute(async (ctx, plugin) => {
+        await plugin.onAfterCommand(ctx, env);
+      });
+    }
   }
 
   /**
