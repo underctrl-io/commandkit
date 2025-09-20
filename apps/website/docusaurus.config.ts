a912a1e8848a492582c928d53e041fdfa2f7fcc9
Avraj <avraj@underctrl.io>
2025-09-20 18:45:26 +0300

    update license

--- apps/website/docusaurus.config.ts
+++ apps/website/docusaurus.config.ts
@@ -175,7 +175,7 @@ const config: Config = {
       </a>
       <br/>
       <br/>
-      GPL-3.0-or-later © ${new Date().getFullYear()} Avraj Sahota`,
+      GPL-3.0-only © ${new Date().getFullYear()} Avraj Sahota`,
     },
     prism: {
       theme: prismThemes.vsLight,
